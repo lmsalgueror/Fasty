1741213e2ff69176878d977da31c52637025cbc8
Lina María Salguero <lmsalgueromd@gmail.com>
2022-12-15 11:05:27 -0500

    First deploy

---
+++ menuData.js
@@ -0,0 +1,97 @@
+export const menuData = [{
+        name: "McFlurry Oreo",
+        price: 9500,
+        text: "Helado de vainilla con topping de oreo y salsa de chocolate",
+        category: "postre",
+        img: "img/mcflurryoreo.png",
+        code: "mc-oreo"
+    },
+    {
+        name: "McFlurry m&m's",
+        price: 9500,
+        text: "Mcflurry con helado de vainilla, topping de m&m y salsa de arequipe",
+        category: "postre",
+        img: "img/mcflurrymym.png",
+        code: "mc-mym"
+    },
+    {
+        name: "Papas grandes",
+        price: 4500,
+        text: "Deliciosas papas crujientes grandes",
+        category: "otro",
+        img: "img/papasgrande.png",
+        code: "papas-grandes"
+    },
+    {
+        name: "Mccombo Grande Big Mac",
+        price: 28900,
+        text: "Elige tu mccombo™ con hamburguesa con dos carnes de res de 50gr c/u, cebolla, pepinillos, lechuga, queso cheddar, pan en el centro y nuestra salsa especial big mac con papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/bigmac.png",
+        code: "big-mac"
+    },
+    {
+        name: "Mccombo Grande Mcnuggets X 11",
+        price: 27900,
+        text: "Elige tu mccombo™ de 11 piezas de pollo apanados acompañados de papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/mcnuggets.png",
+        code: "mc-nuggets"
+    },
+    {
+        name: "Mccombo Grande Mcnifica",
+        price: 28900,
+        text: "Elige tu mccombo™ de hamburguesa con carne de res de 125gr, queso cheddar, lechuga, tomate, cebolla, salsa de tomate, mayonesa y mostaza con papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/mcnifica.png",
+        code: "mc-nifica"
+    },
+    {
+        name: "Mccombo Grande Doble Cuarto De Libra",
+        price: 34900,
+        text: "Elige tu mccombo™ de hamburguesa con dos carnes de res de 125gr c/u, pepinillos, cebolla, doble queso cheddar, salsa de tomate y mostaza con papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/doblecuarto.png",
+        code: "cuarto-libra"
+    },
+    {
+        name: "Mccombo Grande Mcbacon",
+        price: 28900,
+        text: "Elige tu mccombo™ de hamburguesa con dos carnes de res de 50gr c/u, tocineta, queso cheddar, cebolla, salsa de tomate y mostaza con papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/mcbacon.png",
+        code: "mc-bacon"
+    },
+    {
+        name: "Mccombo Grande Mcbacon Triple",
+        price: 31900,
+        text: "Elige tu mccombo™ de hamburguesa con tres carnes de res de 50gr c/u, tocineta, queso cheddar, cebolla, salsa de tomate y mostaza con papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/mcbacontriple.png",
+        code: "mc-bacontriple"
+    },
+    {
+        name: "Mccombo Grande Triple Queso",
+        price: 27900,
+        text: "Elige tu mccombo™ de hamburguesa con tres carne de 50gr c/u, doble queso cheddar, cebolla, pepinillos, salsa de tomate y mostaza con papas grandes y gaseosa grande a elegir",
+        category: "combo",
+        img: "img/doblecuarto.png",
+        code: "triple-queso"
+    },
+    {
+        name: "Cajita Feliz Hamburguesa Sencilla",
+        price: 19900,
+        text: "Elige tu cajita feliz™ con hamburguesa sencilla, papas kids, del valle moraah o mangooh de 200ml, yogo yogo melocotón y juguete",
+        category: "combo",
+        img: "img/cajitafelizhamburguesa.png",
+        code: "cajita-hamburguesa"
+    },
+    {
+        name: "Cajita Feliz Mcnuggets 4 Piezas",
+        price: 19900,
+        text: "Elige tu cajita feliz™ con mcnuggets de 4 piezas, papas kids, del valle moraah o mangooh de 200ml, yogo yogo melocotón y juguete",
+        category: "combo",
+        img: "img/cajitafeliznuggets.png",
+        code: "cajita-nuggets"
+    },
+]
